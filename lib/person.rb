c76f39463ce7c7863d59fa53725f93c15bf645c8
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-10-15 21:51:51 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,11 +4,10 @@ class Person
   
   def initialize(attributes=nil)
     if attributes
-      attributes.each do { |key, value| 
+      attributes.each { |key, value| 
     self.send ("#{key}=", value) }
+    end
   end
-end
-end
   
   
   
